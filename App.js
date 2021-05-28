1dd9dc53371e86476d497a3b7481a5fd1120061d
Isaiah Colson <colson.isaiah@gmail.com>
2021-05-28 14:20:55 -0500

    add basic styling objects

--- App.js
+++ App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-
-const styles = StyleSheet.create({
-  center: {
-    alignItems: 'center',
-  },
-});
-
-const Greeting = props => {
-  return (
-    <View style={styles.center}>
-      <Text>Hello {props.name},</Text>
-      <Text>here is your Life List</Text>
-    </View>
-  );
-};
+import React, {Fragment} from 'react';
+import {SafeAreaView, View} from 'react-native';
+import theme from './src/styles/theme.style';
+import StatusBarColor from './src/components/StatusBar';
+import HomeGreeting from './src/components/homeScreen/homeGreeting';
 
 const LifeListApp = () => {
   return (
-    <View style={[styles.center, {top: 50}]}>
-      <Greeting name="Kenny" />
-    </View>
+    <Fragment>
+      <StatusBarColor
+        backgroundColor={theme.blue_jay_white}
+        barStyle="dark-content"
+      />
+      <SafeAreaView>
+        <View>
+          <HomeGreeting name="Kenny" />
+        </View>
+      </SafeAreaView>
+    </Fragment>
   );
 };
 
