aed399d88cf5c5d99dc266b5cb5cbd1254f08e1c
Isaiah Colson <61123237+isaiahcolson@users.noreply.github.com>
2021-05-29 16:55:00 -0500

    create a hard-coded home screen (#18)
    * create a hard-coded home screen
    
    * 0.1.1

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2,16 +2,36 @@ import {StyleSheet} from 'react-native';
 import theme from './theme.style';
 
 const styles = StyleSheet.create({
+  // header options
+  largeHeaderBold: {
+    color: theme.blue_jay_black,
+    fontSize: theme.font_size_13,
+    fontWeight: theme.font_weight_bold,
+  },
   mediumHeaderBold: {
     color: theme.blue_jay_black,
+    fontSize: theme.font_size_6,
     fontWeight: theme.font_weight_bold,
-    fontSize: theme.font_size_7,
   },
-  mediumHeaderNormal: {
+  mediumHeaderRegular: {
     color: theme.blue_jay_black,
-    fontWeight: theme.font_weight_normal,
-    fontSize: theme.font_size_7,
-    fontFamily: 'sans serif',
+    fontSize: theme.font_size_6,
+    fontWeight: theme.font_weight_regular,
+  },
+  smallParagraphRegular: {
+    color: theme.blue_jay_black,
+    fontSize: theme.font_size_2,
+    fontWeight: theme.font_weight_regular,
+  },
+  // general styles
+  centeredComponent: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
