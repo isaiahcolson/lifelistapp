9dc440e54b02544acd8859be06979a576e943f8e
Isaiah Colson <61123237+isaiahcolson@users.noreply.github.com>
2021-05-31 20:03:21 -0500

    setup google auth (#28)
    * setup google auth
    
    * 0.3.0

--- src/navigation/AuthStackScreen.js
+++ src/navigation/AuthStackScreen.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 import SigninScreen from '../screens/SigninScreen';
 import SignupScreen from '../screens/SignupScreen';
+import {GOOGLE_TOKEN} from '@env';
 
 const AuthStack = createStackNavigator();
 
-const AuthStackScreen = () => (
-  <AuthStack.Navigator>
-    <AuthStack.Screen
-      name="Sign In"
-      component={SigninScreen}
-      options={{headerShown: false}}
-    />
-    <AuthStack.Screen
-      name="Sign Up"
-      component={SignupScreen}
-      options={{title: ''}}
-    />
-  </AuthStack.Navigator>
-);
+const AuthStackScreen = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: `${GOOGLE_TOKEN}`,
+    });
+  });
+
+  return (
+    <AuthStack.Navigator>
+      <AuthStack.Screen
+        name="Sign In"
+        component={SigninScreen}
+        options={{headerShown: false}}
+      />
+      <AuthStack.Screen
+        name="Sign Up"
+        component={SignupScreen}
+        options={{title: ''}}
+      />
+    </AuthStack.Navigator>
+  );
+};
 
 export default AuthStackScreen;
