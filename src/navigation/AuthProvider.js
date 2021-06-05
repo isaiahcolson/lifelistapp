28933c34130560b407910a4da643206a7b16601d
Isaiah Colson <61123237+isaiahcolson@users.noreply.github.com>
2021-06-04 21:24:34 -0500

    discovery - add a user to firebase collection (#43)

--- src/navigation/AuthProvider.js
+++ src/navigation/AuthProvider.js
@@ -1,6 +1,7 @@
 import React, {createContext, useContext, useEffect, useState} from 'react';
 import auth from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import firestore from '@react-native-firebase/firestore';
 
 export const authContext = createContext();
 
@@ -50,6 +51,27 @@ function useProvideAuth() {
       .createUserWithEmailAndPassword(email, password)
       .then(response => {
         setUser(response.user);
+        firestore()
+          .collection('users')
+          .add({
+            displayName: response.user.displayName,
+            email: response.user.email,
+            emailVerified: response.user.emailVerified,
+            phoneNumber: response.user.phoneNumber,
+            photoURL: response.user.photoURL,
+            uid: response.user.uid,
+            birdData: {
+              favoriteBird: '',
+              lifeList: [],
+            },
+            metadata: {
+              creationTime: response.user.metadata.creationTime,
+              lastSignInTime: response.user.metadata.lastSignInTime,
+            },
+          })
+          .then(() => {
+            console.log('User added!');
+          });
         return response.user;
       });
   };
@@ -57,7 +79,7 @@ function useProvideAuth() {
   const updateUser = async newValues => {
     try {
       await auth().currentUser.updateProfile(newValues);
-      await setUser(auth().currentUser);
+      setUser(auth().currentUser);
     } catch (error) {
       console.log(error);
     }
